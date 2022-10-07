fbd011e30634771eaf5c9d889e675e871239e8d4
jihoo1018 <wendy1991018@gmail.com>
2022-10-07 17:27:13 +0900

    10/07

---
+++ 정렬/버블 정렬 .py
@@ -0,0 +1,37 @@
+
+import random
+
+class Solution:
+    def solution(self):
+        keys = ['1st','2nd', '3rd','4th','5th']
+        dc={}
+        print('### 랜덤 숫자 5개 생성 ###')
+        print("*" * 30)   
+        for i in keys :    
+            dc[i] = random.randint(1,100)
+        for k,v in dc.items(): 
+            print(f"랜덤숫자{k}: {v}")
+        print("*" * 30)
+
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    solution.solution()
+
+
+
+import random
+class Solution:
+    def solution(self):
+        keys = ['1st','2nd','3rd','4th','5th']
+        dc = {}
+        for i in keys:
+            dc[i] = random.randint(1,11)
+        for k, v in dc.items():
+            print(f'랜덤 숫자{k} : {v}')
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.solution())
