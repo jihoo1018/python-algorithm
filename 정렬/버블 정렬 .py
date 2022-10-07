6572c961da649a27c739284e7ca35ed1f7ad4454
jihoo1018 <wendy1991018@gmail.com>
2022-10-07 17:42:27 +0900

    10/17

--- 정렬/버블 정렬 .py
+++ 정렬/버블 정렬 .py
@@ -20,18 +20,3 @@ if __name__ == "__main__":
     solution = Solution()
     solution.solution()
 
-
-
-import random
-class Solution:
-    def solution(self):
-        keys = ['1st','2nd','3rd','4th','5th']
-        dc = {}
-        for i in keys:
-            dc[i] = random.randint(1,11)
-        for k, v in dc.items():
-            print(f'랜덤 숫자{k} : {v}')
-
-if __name__ == "__main__":
-    solution = Solution()
-    print(solution.solution())
